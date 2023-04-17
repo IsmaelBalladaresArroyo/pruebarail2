ef9db4d1f1734dcb161098917ecfc5e77c45faad
IsmaelBalladaresArroyo <ismael.balladares@tecsup.edu.pe>
2023-04-16 19:57:51 -0500

    version commit

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-41.railway.app";
-	$port = "port=6953";
+	$host = "host=containers-us-west-198.railway.app";
+	$port = "port=7377";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=gx48a5ALGCFUHSn4bfGb";
+	$password = "password=0HbTRyk5Kxr9xjNAldaL";
 	$db = pg_connect("$host $port $dbname $user $password");
     return $db;
 }
